be35f677fbaaa7e1d6ec67552223278c00ac64ce
Mads Pedersen <mads456dk@gmali.com>
2024-02-01 13:13:35 +0100

    Added Exercise

---
+++ src/main/java/vop/PrimaryController.java
@@ -0,0 +1,18 @@
+package vop;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    public void initialize() {
+//        ellipseRadio.setUserData(ShapeFacade.SHAPES.ELLIPSE);
+//        rectangleRadio.setUserData(ShapeFacade.SHAPES.RECTANGLE);
+//        circleRadio.setUserData(ShapeFacade.SHAPES.CIRCLE);
+//        squareRadio.setUserData(ShapeFacade.SHAPES.SQUARE);
+    }
+    
+}
